f461f5ffb40b96cc075c58b47367c28381d509c2
Khairul Makirin <khairul240999@gmail.com>
2018-10-13 12:10:05 +0700

    Selesai

---
+++ Praktikum 7 (Stack)/Pra Praktikum/stackt.c
@@ -0,0 +1,48 @@
+#include "stackt.h"
+#include "boolean.h"
+
+
+
+void CreateEmpty (Stack *S){
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+    TOP(*S)=Nil;
+}
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty (Stack S){
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+
+    return (TOP(S)==Nil);
+}
+
+boolean IsFull (Stack S){
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+
+    return (TOP==MaxEL+1);
+}
+
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack * S, infotype X){
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+    TOP(*S)++;
+    InfoTop(*S)=X;
+}
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack * S, infotype* X){
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+
+    (*X)=InfoTop(*S);
+    TOP(*S)--;
+
+}
